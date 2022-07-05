8185a0d1471f45380db8a636985c5c5bf3271fda
gdsohag360 <gdsohag360@gmail.com>
2022-07-05 09:24:50 -0700

    add Najul page

---
+++ src/Najmul.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function componentName() {
+    return (
+        <div>
+            <h1>i am Najmul </h1>
+            <span>i have a many gf you know this ?</span>
+            <p>but i always say i don't gf, this is false . real news i have many gf that not countable</p>
+            <span>🤣😂😂🤣🤣🤣🤣😂😂</span>
+        </div>
+    )
+}
+
+export default componentName
